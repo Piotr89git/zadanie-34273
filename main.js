1c370bcde949a6331bec3e6a456ee1d4b2127b96
Piotr Czernecki <czernecki.piotr@gmail.com>
2021-12-16 21:51:56 +0100

    zadanie-34273

---
+++ main.js
@@ -0,0 +1,11 @@
+let firstNumber = 7;
+let secondNumber = 10;
+
+function addition(num1, num2) {
+    let suma = num1 + num2;
+    
+    return suma; 
+    }
+    console.log( addition (7, 10,) );
+
+
